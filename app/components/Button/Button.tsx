671aaaec7c1f2bb2924d5e01b5e8dc33a5e0f4e0
Kuba <jakub.jaszcz538@gmail.com>
2025-03-19 22:54:22 +0100

    feat:Buttons and Navbar

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,13 +1,19 @@
-import Link from "next/link"
+import Link from "next/link";
 
-type BtnProps={
-    link:string,
-    isActive?:boolean,
-    text:string
-}
-export default function Button({link,isActive=false,text}:BtnProps){
-    return(
-    <Link href={link} className={`rounded-sm ${isActive ? 'bg-green-400':''}`} >
-        {text}
-    </Link>)
+type BtnProps = {
+  link: string;
+  isActive?: boolean;
+  text: string;
+};
+export default function Button({ link, isActive = false, text }: BtnProps) {
+  return (
+    <Link
+      href={link}
+      className={`text-center rounded-full font-bold border-slate-800 border-2 px-8 py-4 text-xl transition-all hover:shadow-lg ${
+        isActive ? "bg-green-400" : ""
+      }`}
+    >
+      {text}
+    </Link>
+  );
 }
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
+import Logo from "@/app/assets/logo.svg";
+import Link from "next/link";
+import Button from "../Button/Button";
 
-export default function Navbar({}){
-    return( 
-    <nav className="">
-        <h1>TrackMyWealth tu będzie logo btw</h1>
-    </nav>)
+const links = ["Product", "Services", "Pricing", "About"];
+
+export default function Navbar({}) {
+
+  return (
+    <nav className="flex items-center gap-12 justify-between px-6 py-12">
+      <Image src={Logo} className="w-1/3 md:w-1/2" alt="" />
+      <ul className="flex gap-6">
+        <li>
+          <Link className="text-xl md:text-md" href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link className="text-xl" href={"/product"}>Product</Link>
+        </li>
+        <li>
+          <Link className="text-xl" href={"/services"}>Services</Link>
+        </li>
+        <li>
+          <Link className="text-xl" href={"/about"}>About</Link>
+        </li>
+      </ul>
+      <Button link="/register" text="Get Started"/>
+    </nav>
+  );
 }
